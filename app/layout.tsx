0fa94799afc1b76983a8244a4290b24a326ea21a
mia <bussinessmia1@gmail.com>
2026-05-18 17:23:55 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "NSN MULTIWATCH",
   description: "NSN Multiwatch Community",
+  icons: {
+    icon: "/nsnlogo.png",
+  },
 };
 
 export default function RootLayout({
